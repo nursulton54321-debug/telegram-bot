7cd8e3d387f0229e2f0ba1355b0326f44a26347d
nursulton54321-debug <nursulton54321@gmail.com>
2026-03-15 22:00:54 +0500

    bot.php

--- bot.php
+++ bot.php
@@ -4,22 +4,22 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // BOT TOKEN
-$token = "8504597068:AAH1Gxh5aoHgls8jVZ3boSVUUmtpLM4cPEw";
+$token = "SIZNING_YANGI_TOKEN";
 
 // Telegram update olish
 $update = json_decode(file_get_contents("php://input"), true);
 
-// Agar message bo‘lsa
+// Agar message mavjud bo‘lsa
 if(isset($update["message"])){
 
     $chat_id = $update["message"]["chat"]["id"];
     $text = $update["message"]["text"];
 
-    // Javob matni
-    $reply = "Bot Railway serverda ishlayapti ✅";
+    $reply = "Bot ishlayapti ✅";
 
-    // Telegramga javob yuborish
-    file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chat_id."&text=".urlencode($reply));
+    file_get_contents(
+        "https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chat_id."&text=".urlencode($reply)
+    );
 }
 
 ?>
