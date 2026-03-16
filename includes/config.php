4e10143e9a4b150a6ce45f179e400a94a42e2e8a
nursulton54321-debug <nursulton54321@gmail.com>
2026-03-16 22:03:52 +0500

    config.php

--- includes/config.php
+++ includes/config.php
@@ -1,21 +1,14 @@
 <?php
 
-ini_set('display_errors',1);
-error_reporting(E_ALL);
-
 $DB_HOST = getenv('MYSQLHOST');
 $DB_USER = getenv('MYSQLUSER');
 $DB_PASS = getenv('MYSQLPASSWORD');
 $DB_NAME = getenv('MYSQLDATABASE');
-$DB_PORT = getenv('MYSQLPORT') ?: 3306;
+$DB_PORT = getenv('MYSQLPORT');
 
 function dbConnect(){
 
     global $DB_HOST,$DB_USER,$DB_PASS,$DB_NAME,$DB_PORT;
-
-    if(!$DB_HOST){
-        die("Database ENV variables topilmadi");
-    }
 
     $conn = new mysqli(
         $DB_HOST,
